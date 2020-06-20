8d4c98a175b2c3d2445cbcbbb0769a6a452ba0b8
Safaa125 <safaaalraheem@hotmail.com>
2020-06-20 19:38:55 +1000

    Added the skeleton print structures

--- skeleton.py
+++ skeleton.py
@@ -1,7 +1,20 @@
+
 #Introduction
-
+class intro():
+    def mainIntro():
+        print("Welcome to the quiz!\nThere are three topics choose from.")
 #Choice of Topics
-
+    choice = input("\nCrypto, Economics or St John?\n>")
+    if choice == "Crypto":
+        print("Chosen Crypto")
+    elif choice == "Economics":
+        print("Chosen Economics")
+    elif choice == "St John":
+        print("Chosen Medic")
+    else:
+        print("That is not a valid choice, try again")
+        
+        
 #Timer
 
 #Right/Wrong total
@@ -11,3 +24,4 @@
 #Questions from Topic 2
 
 #Questions from Topic 3
+intro()
