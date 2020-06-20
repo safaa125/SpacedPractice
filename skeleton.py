90fe476cd532c1b27bf7e98219e3f8b199b05cdd
Safaa125 <safaaalraheem@hotmail.com>
2020-06-20 16:09:53 +1000

    Outline of Skeleton

--- skeleton.py
+++ skeleton.py
@@ -1,5 +1,13 @@
-print("Hello Habibis")
-print("I am updating this with a new information")
-print("added on a new branch")
-print("more updates")
-print("let me update pls")
+#Introduction
+
+#Choice of Topics
+
+#Timer
+
+#Right/Wrong total
+
+#Questions from Topic 1
+
+#Questions from Topic 2
+
+#Questions from Topic 3
